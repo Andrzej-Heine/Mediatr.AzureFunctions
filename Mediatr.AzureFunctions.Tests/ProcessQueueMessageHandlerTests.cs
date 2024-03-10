cafa37ffae705f7e87f5fcf880a09f1ddf10b344
Andrzej Heine <andrzej.heine@gmail.com>
2024-03-10 13:38:43 +0100

    Added a new test to the class called ProcessQueueMessageHandlerTests.cs

--- Mediatr.AzureFunctions.Tests/ProcessQueueMessageHandlerTests.cs
+++ Mediatr.AzureFunctions.Tests/ProcessQueueMessageHandlerTests.cs
@@ -2,6 +2,7 @@
 using IsolatedMediatr.Validators;
 using System.Collections;
 using IsolatedMediatr.Handlers;
+using IsolatedMediatr.Models;
 
 namespace Mediatr.AzureFunctions.Tests
 {
@@ -28,16 +29,19 @@ namespace Mediatr.AzureFunctions.Tests
          Assert.StartsWith("[Invalid Json object]", result);
       }
 
-      private class QueueMessageRequestTestData_InvalidJsonObject : IEnumerable<object[]>
+      [Fact]
+      public async Task ProcessQueueMessageHandler_ValidateJson_CorrectJsonObject()
       {
-         public IEnumerator<object[]> GetEnumerator()
-         {
-            yield return new object[] { new QueueMessageRequest() { QueueMessage = "" } };
-            yield return new object[] { new QueueMessageRequest() { QueueMessage = string.Empty } };
-            yield return new object[] { new QueueMessageRequest() { QueueMessage = null } };
-         }
+         //Arrange
+         var queueMessageRequest = new QueueMessageRequest()
+         { QueueMessage = "{\"Name\": \"Andrzej Heine\",\"Email\": \"andrzej.heine@gmail.com\"}" };
+         var handler = new ProcessQueueMessageHandler(_personValidator);
 
-         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+         //Act
+         var result = await handler.Handle(queueMessageRequest, new CancellationToken());
+
+         //Assert
+         Assert.DoesNotContain("[Invalid Json object]", result);
       }
 
       [Theory]
@@ -54,6 +58,21 @@ namespace Mediatr.AzureFunctions.Tests
          Assert.StartsWith("[Invalid person object]", result);
       }
 
+      [Fact]
+      public async Task ProcessQueueMessageHandler_PersonValidator_CorrectJsonObject()
+      {
+         //Arrange
+         var queueMessageRequest = new QueueMessageRequest()
+         { QueueMessage = "{\"Name\": \"Andrzej Heine\",\"Email\": \"andrzej.heine@gmail.com\"}" };
+         var handler = new ProcessQueueMessageHandler(_personValidator);
+
+         //Act
+         var result = await handler.Handle(queueMessageRequest, new CancellationToken());
+
+         //Assert
+         Assert.DoesNotContain("[Invalid person object]", result);
+      }
+
       private class QueueMessageRequestTestData_InvalidPersonObject : IEnumerable<object[]>
       {
          public IEnumerator<object[]> GetEnumerator()
@@ -63,6 +82,19 @@ namespace Mediatr.AzureFunctions.Tests
             yield return new object[] { new QueueMessageRequest() { QueueMessage = "{ \"Namev2\" : \"Andrzej Heine\"} " } };
             yield return new object[] { new QueueMessageRequest() { QueueMessage = "{ \"Email\" : \"Andrzej.Heine@gmail.com  \"} " } };
             yield return new object[] { new QueueMessageRequest() { QueueMessage = "{ \"Emailv2\" : \"Andrzej.Heine@gmail.com  \"} " } };
+         }
+
+         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+      }
+
+      private class QueueMessageRequestTestData_InvalidJsonObject : IEnumerable<object[]>
+      {
+         public IEnumerator<object[]> GetEnumerator()
+         {
+            yield return new object[] { new QueueMessageRequest() { QueueMessage = "" } };
+            yield return new object[] { new QueueMessageRequest() { QueueMessage = string.Empty } };
+            yield return new object[] { new QueueMessageRequest() { QueueMessage = null } };
+            yield return new object[] { new QueueMessageRequest() { QueueMessage = "abc" } };
          }
 
          IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
